e0c809fd190904b784060a539f191ad8e245645e
florence96 <projin1234@gmail.com>
2021-08-26 13:59:02 +0900

    간단한 입출력 테스트

---
+++ Algorithmn_C++/Concepts/Algorithmn Programming Tutorial/basic.cpp
@@ -0,0 +1,20 @@
+//
+//  basic.cpp
+//  Algorithmn_C++
+//
+//  Created by Yejin Hong on 2021/08/26.
+//
+#include <iostream>
+using namespace std;
+
+// 간단한 출력
+void output(){
+    cout << "Hello World\n";
+}
+
+// 간단한 입력
+void input(){
+    int x;
+    cin >> x;
+    printf("the number is %d\n", x);
+}
